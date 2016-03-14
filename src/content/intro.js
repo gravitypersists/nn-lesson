1d99ccd8c8481338f1762b2c6b87a1f172a4ace4
Michael Silveira <gravitypersists@gmail.com>
2016-03-14 01:16:30 -0700

    introduce performance function

--- src/content/intro.js
+++ src/content/intro.js
@@ -6,6 +6,7 @@ const w = ktx('\\vec{w}')
 const x = ktx('\\vec{x}')
 const z = ktx('\\vec{z}')
 const d = ktx('\\vec{d}')
+const P = ktx('P')
 
 const body = `
   A neural network is like a black box. You give input, and depending on its configuration, you get output.
@@ -18,10 +19,14 @@ const body = `
 
   That is, ${z} is a function of ${x} and ${w}.
 
-  Now suppose we have a desired function, that we wish the neural network to represent
+  Now suppose we have a desired function, ${d}, based on the same input, ${x}, that we wish the neural network to approximate.
 
   --- ${ ktx('\\vec{d} = f(\\vec{x})') }
 
-  It is then clear that our task is to configure ${ ktx('\\vec{w}') } such that ${ ktx('\\vec{z} \\approx \\vec{d}') }.
+  It is clear, then, that our task is to configure ${ ktx('\\vec{w}') } such that ${ ktx('\\vec{z} \\approx \\vec{d}') }. We can devise a function to represent the deviation between these two vectors, called the performance function, ${ P }, which we arbitrarily define as:
+
+  --- ${ ktx('P = f(\\vec{d}, \\vec{z}) = -\\tfrac{1}{2}(\\vec{d}-\\vec{z})^2') }
+
+  As ${z} approaches ${d}, ${P} approaches 0. Ultimately, we want to minimize ${P}, and we do so by adjusting the weights, ${w}, which ${z} is a function of.
 `
 export default body
